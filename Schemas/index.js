4867bd23f57226f633f9e2022963718d5e39e5ce
shashankgupta <shashank.gupta006@gmail.com>
2025-12-11 13:59:18 +0530

    Added login, register APIs and dotenv

--- Schemas/index.js
+++ Schemas/index.js
@@ -12,10 +12,16 @@ const {
 // Import data models for products and orders.
 const  {Product}  = require("../models/products");
 const  {Order} = require("../models/orders");
+const  {User} = require("../models/users");
 
 const ProductType = require("./TypeDefs/ProductType");
 const UserType = require("./TypeDefs/UserType");
 const OrderType = require("./TypeDefs/OrderType");
+
+const dotenv = require('dotenv');
+dotenv.config('.env')
+
+const jwt = require('jsonwebtoken');
 
 // Define the RootQuery, which is the entry point for querying data.
 const RootQuery = new GraphQLObjectType({
@@ -52,6 +58,24 @@ const RootQuery = new GraphQLObjectType({
         }
         const orderList = await Order.find({ userId: args.id });
         return orderList;
+      },
+    },
+    getUser: {
+      type: UserType,
+      args: {
+        username: {type: GraphQLString}
+      },
+      async resolve(parent, args) {
+        const user = await User.findOne({username: args.username});
+        return user
+      }
+    },
+    getAllUsers: {
+       type: new GraphQLList(UserType),
+       args: { id: { type: GraphQLString } },
+        async resolve(parent, args, req) {
+        const userList = await User.find({ id: args.id });
+        return userList;
       },
     },
   },
@@ -146,6 +170,48 @@ const Mutation = new GraphQLObjectType({
         return args;
       },
     },
+    registerUser: {
+      type: GraphQLString,
+      args: {
+        username: { type: GraphQLString },
+        email: { type: GraphQLString },
+        password: { type: GraphQLString },
+        isAdmin: { type: graphql.GraphQLBoolean },
+      },
+      async resolve(parent, args, req) {
+        console.log("Register resolve called " + JSON.stringify(args))
+        const newUser = new User({
+          username: args.username,
+          email: args.email,
+          password: args.password,
+          isAdmin: args.isAdmin
+        });
+        try {
+          await newUser.save()
+          return 'Registration successful';
+        } catch(error) {
+          throw new Error('Registration failed');
+        }
+      }
+    },
+    login: {
+      type: GraphQLString,
+      args: {
+        username: { type: GraphQLString },
+        password: { type: GraphQLString },
+      },
+      resolve: async (parent, args) => {
+        const user = await User.findOne({ username: args.username });
+        if (!user) throw new Error('User not found');
+        if (user.password !== args.password) throw new Error('Incorrect password');
+        const payload = { sub: user.id };
+        const token = jwt.sign(payload, process.env.SECRET_KEY, { expiresIn: '1h' });
+        return token;
+      },
+      reject: async (error) => {
+        console.log(`Login error ${JSON.stringify(error)}`)
+      }
+    }
 }});
 
 // Export a GraphQLSchema that includes the RootQuery.
